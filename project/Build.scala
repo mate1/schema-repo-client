9441d5196aa2561fbeff2c2ff6b51067aa36411b
Felix GV <felix@mate1inc.com>
2014-03-28 19:11:56 -0400

    Tweaked the build so it depends on the Mate1 Public Maven Repo for the AVRO-1124 dependency...

--- project/Build.scala
+++ project/Build.scala
@@ -12,10 +12,14 @@ object ApplicationBuild extends Build {
     scalaVersion        := scalaVersionValue,
     libraryDependencies := Seq(
       "org.apache.avro" % "avro" % "1.7.5",
+      "org.apache.avro.repo" % "avro-repo-client" % "1.7.5-1124-SNAPSHOT",
       "org.scala-lang" % "scala-library" % scalaVersionValue,
+      "com.google.code.findbugs" % "jsr305" % "1.3.9",
       "com.google.guava" % "guava" % "14.0.1",
-      "javax.inject" % "javax.inject" % "1",
-      "com.google.code.findbugs" % "jsr305" % "1.3.+"
+      "javax.inject" % "javax.inject" % "1"
+    ),
+    resolvers ++= Seq(
+      "Mate1 Public Maven Repo" at "https://raw.github.com/mate1/maven/master/public/"
     )
   )
 
